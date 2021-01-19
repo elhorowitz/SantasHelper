63bf5decd1944ab143226d09123ad4d4afa2a4f5
elhorowitz <ericalhorowitz@gmail.com>
2021-01-19 12:25:50 -0500

    use hooks for App

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './login/login';
@@ -7,39 +7,37 @@ import PersonalWishLIst from './personalWishList/personalWishList';
 import PrivateRoute from './components/privateRoute/privateRoute';
 import { isLoggedIn, logIn } from './services/backend';
 
-class App extends React.Component {
-  constructor(...props) {
-    super(...props);
-    this.state = { isAuthenticated: false, user: {}, token: '' };
-  }
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  componentDidMount() {
-    isLoggedIn().then((isAuthenticated) => this.setState({ isAuthenticated }));
-  }
+  useEffect(() => {
+    isLoggedIn().then((result) => setIsAuthenticated(result.isAuthenticated));
+  }, []);
 
-  render() {
-    const authObject = {
-      isAuthenticated: this.state.isAuthenticated,
-      authenticate: () => logIn(),
-    };
-    return (
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login authentication={authObject} />
-          </Route>
+  const authObject = {
+    isAuthenticated,
+    authenticate: () => logIn(),
+  };
 
-          <PrivateRoute authentication={authObject} path="/family">
-            <FamilyWishLIst />
-          </PrivateRoute>
+  console.log(authObject);
 
-          <PrivateRoute authentication={authObject} path="/personal">
-            <PersonalWishLIst />
-          </PrivateRoute>
-        </Switch>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login authentication={authObject} />
+        </Route>
+
+        <PrivateRoute authentication={authObject} path="/family">
+          <FamilyWishLIst />
+        </PrivateRoute>
+
+        <PrivateRoute authentication={authObject} path="/personal">
+          <PersonalWishLIst />
+        </PrivateRoute>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
